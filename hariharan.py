9d09d2decaabd9472fe38c2567700132dbd8d265
hari <harih47@gmail.com>
2022-10-11 20:48:08 +0530

    python code

---
+++ hariharan.py
@@ -0,0 +1,24 @@
+from abc import ABC,abstractmethod
+
+class hari:
+    @abstractmethod
+    def pro(self):
+       pass
+
+    @abstractmethod
+    def pro1(self):
+        pass
+class sahaj:
+    def pro(self):
+        print("my name is hariharan")
+    def pro1(self):
+        print("my name is roshan")
+
+obj=sahaj()
+obj.pro()
+obj.pro1()
+
+
+
+
+
